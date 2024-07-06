ba4abd68b1d256d9a0d159f2d7a0ed6b2b8a7ae2
Iris Raine <iris.raine.darkest.star@gmail.com>
2024-07-06 18:14:55 +0300

    add required abstract methods to interface

--- src/main/java/org/aston/homework2/collections/MyList.java
+++ src/main/java/org/aston/homework2/collections/MyList.java
@@ -1,4 +1,11 @@
 package org.aston.homework2.collections;
 
-public interface MyList {
+public interface MyList<E extends Comparable<E>> {
+    void add(E elem);
+    void addAll(MyList<E> otherList);
+    void size();
+    E get(int index);
+    E get(E elem);
+    E remove(int index);
+    E remove(E elem);
 }
